6947bf199ba81553e468bc050d89b77b16a271f7
Ilja Gerasimov <geras.ilja@gmail.com>
2025-12-04 22:13:39 +0100

    refined code

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: igerasim <igerasim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/20 05:17:17 by igerasim          #+#    #+#             */
-/*   Updated: 2025/12/04 21:13:03 by igerasim         ###   ########.fr       */
+/*   Updated: 2025/12/04 22:12:01 by igerasim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 		i = 0;
 		while (i < n)
 		{
-			(((unsigned char *)dest)[i] = ((const unsigned char *)src)[i]);
+			((unsigned char *)dest)[i] = ((const unsigned char *)src)[i];
 			i++;
 		}
 	}
